eb67c3f65242304bfaffea7f7b41ff7febec4c49
naveenkumar <iamnk1000@gmail.com>
2025-06-28 15:14:02 +0530

    Disable LightningCSS to fix Vercel build

--- retrofy-ecommerce/tailwind.config.js
+++ retrofy-ecommerce/tailwind.config.js
@@ -1,12 +1,10 @@
-// tailwind.config.js
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
-  // ⛔ Add this line to disable lightningcss
   future: {
-    useFutureCss: false,
+    useFutureCss: false, // ⛔️ disables LightningCSS
   },
-}
+};
